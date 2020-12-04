24594d75fed4ccafd55a0d87d88030d64bee213e
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-04 14:41:57 -0500

    add recursive for n operands

--- day_01/solution.rb
+++ day_01/solution.rb
@@ -16,7 +16,7 @@ end
 
 def brute_force(numbers)
   numbers.each do |number|
-    pair = numbers.find { |other| (other + number) == 2020 }
+    pair = numbers.find { |other| other != number && (other + number) == 2020 }
     return Result.new(number, pair) if pair
   end
 end
@@ -36,16 +36,37 @@ def remainder_with_hash(numbers)
   end
 end
 
+def recursive(numbers, operand_count = 2, current_operands = [])
+  numbers.each do |number|
+    next if current_operands.include? number
+    new_operands = current_operands + [number]
+    if new_operands.count < operand_count
+      found = recursive(numbers, operand_count, new_operands)
+      return found if found
+    else
+      return Result.new(*new_operands) if new_operands.reduce(:+) == 2020
+    end
+  end
+  false
+end
+
 example = [1721, 979, 366, 299, 675, 1456]
 
-def check_answer(result)
-  raise 'oops' unless result.product == 514579
+def answer_icon(result, operands = 2)
+  answers = {2 => 514579, 3 => 241861950}
+  expected = answers[operands]
+  result.product == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
 
 puts 'Example:'
-brute_force(example).tap { |result| puts "Brute force: #{result}"; check_answer(result) }
-remainder(example).tap { |result| puts "Remainder: #{result}"; check_answer(result) }
-remainder_with_hash(example).tap { |result| puts "Remainder with hash: #{result}"; check_answer(result) }
+puts '2 operands'
+brute_force(example).tap { |result| puts "Brute force: #{result} #{answer_icon(result)}" }
+remainder(example).tap { |result| puts "Remainder: #{result} #{answer_icon(result)}" }
+remainder_with_hash(example).tap { |result| puts "Remainder with hash: #{result} #{answer_icon(result)}" }
+recursive(example).tap { |result| puts "Recursive: #{result} #{answer_icon(result)}" }
+puts ''
+puts '3 operands'
+recursive(example, 3).tap { |result| puts "Recursive: #{result} #{answer_icon(result, 3)}" }
 puts ''
 
 puts 'Input:'
@@ -61,5 +82,13 @@ Benchmark.bm do |benchmark|
 
   benchmark.report('Remainder with hash'.light_blue) do
     puts " (#{remainder_with_hash(numbers)})".green
+  end
+
+  benchmark.report('Recursive 2 operands'.light_blue) do
+    puts " (#{recursive(numbers)})".green
+  end
+
+  benchmark.report('Recursive 3 operands'.light_blue) do
+    puts " (#{recursive(numbers, 3)})".green
   end
 end
