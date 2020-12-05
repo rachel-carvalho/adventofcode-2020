464abb3a9be720f5b780408b8645ab962f255273
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-05 11:24:26 -0500

    find missing seat

--- day_05/solution.rb
+++ day_05/solution.rb
@@ -1,24 +1,39 @@
 require 'benchmark'
 require '../colors'
 
+def get_slice(items, lower_half)
+  if lower_half
+    [items.min, ((items.max - items.min) / 2.0).floor + items.min]
+  else
+    [((items.max - items.min) / 2.0).ceil + items.min, items.max]
+  end
+end
+
 def find_max_seat_id(passes)
   passes.map do |pass|
     row = [0, 127]
     column = [0, 7]
     pass.each_char do |char|
-      if char == 'F'
-        row = [row.min, ((row.max - row.min) / 2.0).floor + row.min]
-      elsif char == 'B'
-        row = [((row.max - row.min) / 2.0).ceil + row.min, row.max]
-      elsif char == 'L'
-        column = [column.min, ((column.max - column.min) / 2.0).floor + column.min]
-      elsif char == 'R'
-        column = [((column.max - column.min) / 2.0).ceil + column.min, column.max]
+      if %w{F B}.include?(char)
+        row = get_slice(row, char == 'F')
+      elsif %w{L R}.include?(char)
+        column = get_slice(column, char == 'L')
       end
     end
     [pass, {row: row.min, column: column.min, id: row.min * 8 + column.min}]
   end.to_h.tap do |result|
     result[:max] = result.values.map {|p| p[:id]}.max
+  end
+end
+
+def find_missing_seat(passes)
+  parsed_passes = find_max_seat_id(passes)
+  parsed_passes.delete :max
+  ids = parsed_passes.values.map {|p| p[:id]}.sort
+  expected = ids.min
+  ids.each do |id|
+    return expected if expected != id
+    expected += 1
   end
 end
 
@@ -45,5 +60,9 @@ input = File.readlines('input')
 Benchmark.bm do |benchmark|
   benchmark.report('Max seat id'.light_blue) do
     puts " (#{find_max_seat_id(input)[:max]})".green
+  end
+
+  benchmark.report('Missing seat'.light_blue) do
+    puts " (#{find_missing_seat(input)})".green
   end
 end
