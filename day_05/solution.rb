e1773373caca67f7dc4f04b8092be9eab8829689
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-05 11:10:17 -0500

    find max seat id

--- day_05/solution.rb
+++ day_05/solution.rb
@@ -2,7 +2,24 @@ require 'benchmark'
 require '../colors'
 
 def find_max_seat_id(passes)
-  0
+  passes.map do |pass|
+    row = [0, 127]
+    column = [0, 7]
+    pass.each_char do |char|
+      if char == 'F'
+        row = [row.min, ((row.max - row.min) / 2.0).floor + row.min]
+      elsif char == 'B'
+        row = [((row.max - row.min) / 2.0).ceil + row.min, row.max]
+      elsif char == 'L'
+        column = [column.min, ((column.max - column.min) / 2.0).floor + column.min]
+      elsif char == 'R'
+        column = [((column.max - column.min) / 2.0).ceil + column.min, column.max]
+      end
+    end
+    [pass, {row: row.min, column: column.min, id: row.min * 8 + column.min}]
+  end.to_h.tap do |result|
+    result[:max] = result.values.map {|p| p[:id]}.max
+  end
 end
 
 def answer_icon(result)
@@ -17,16 +34,16 @@ end
 
 example = 'BFFFBBFRRR
 FFFBBBFRRR
-BBFFBBFRLL'
+BBFFBBFRLL'.split("\n")
 
 puts 'Example:'
 find_max_seat_id(example).tap { |result| puts "Max seat id: #{result} #{answer_icon(result)}" }
 puts ''
 
 puts 'Input:'
-input = File.read('input')
+input = File.readlines('input')
 Benchmark.bm do |benchmark|
   benchmark.report('Max seat id'.light_blue) do
-    puts " (#{find_max_seat_id(input)})".green
+    puts " (#{find_max_seat_id(input)[:max]})".green
   end
 end
