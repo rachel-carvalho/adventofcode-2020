153f0bfee043cbcecdf942527f951e09dc573d70
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-06 17:44:19 -0500

    sum answers by all members of group

--- day_06/solution.rb
+++ day_06/solution.rb
@@ -7,8 +7,15 @@ def sum_unique_answers(input)
   end
 end
 
-def answer_icon(result)
-  expected = 11
+def sum_same_answers(input)
+  input.split("\n\n").sum do |group|
+    people = group.split("\n")
+    people.map(&:chars).flatten.tally.values.count { |answer_count| answer_count == people.count }
+  end
+end
+
+def answer_icon(result, all_same)
+  expected = all_same ? 6 : 11
   result == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
 
@@ -29,13 +36,17 @@ a
 b'
 
 puts 'Example:'
-sum_unique_answers(example).tap { |result| puts "Unique answers: #{result} #{answer_icon(result)}" }
+sum_unique_answers(example).tap { |result| puts "Unique answers: #{result} #{answer_icon(result, false)}" }
+sum_same_answers(example).tap { |result| puts "All same answers: #{result} #{answer_icon(result, true)}" }
 puts ''
 
 puts 'Input:'
 input = File.read('input')
 Benchmark.bm do |benchmark|
-  benchmark.report('Unique answers:'.light_blue) do
+  benchmark.report('Unique answers'.light_blue) do
     puts " (#{sum_unique_answers(input)})".green
+  end
+  benchmark.report('All same answers'.light_blue) do
+    puts " (#{sum_same_answers(input)})".green
   end
 end
