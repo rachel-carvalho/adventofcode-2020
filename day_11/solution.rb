0ec5f39d65678445aeb111da1b8986086819ac1c
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-11 12:52:44 -0500

    look for seats in line of sight

--- day_11/solution.rb
+++ day_11/solution.rb
@@ -1,3 +1,4 @@
+require 'matrix'
 require 'benchmark'
 require '../colors'
 
@@ -19,9 +20,10 @@ end
 def change_occupation(rows, line_of_sight)
   rows.map.with_index do |line, row|
     line.each_char.with_index.map do |spot, column|
+      max_occupied = line_of_sight ? 5 : 4
       if spot == 'L' && count_occupied_around(rows, row, column, line_of_sight).zero?
         '#'
-      elsif spot == '#' && count_occupied_around(rows, row, column, line_of_sight) >= 4
+      elsif spot == '#' && count_occupied_around(rows, row, column, line_of_sight) >= max_occupied
         'L'
       else
         spot
@@ -34,10 +36,6 @@ def count_occupied_around(rows, row, column, line_of_sight)
   line_of_sight ? count_occupied_line_of_sight(rows, row, column) : count_occupied_adjacent(rows, row, column)
 end
 
-def count_occupied_line_of_sight(rows, row, column)
-  0
-end
-
 def count_occupied_adjacent(rows, row, column)
   count = 0
   ((row - 1)..(row + 1)).each do |adjacent_row|
@@ -46,6 +44,38 @@ def count_occupied_adjacent(rows, row, column)
       if neighbor && adjacent_row >= 0 && adjacent_column >= 0 && adjacent_row < rows.count && adjacent_column < rows[0].length
         count += 1 if rows[adjacent_row][adjacent_column] == '#'
       end
+    end
+  end
+  count
+end
+
+def count_occupied_line_of_sight(rows, row, column)
+  directions = {
+    n: Vector[-1, 0],
+    ne: Vector[-1, 1],
+    e: Vector[0, 1],
+    se: Vector[1, 1],
+    s: Vector[1, 0],
+    sw: Vector[1, -1],
+    w: Vector[0, -1],
+    nw: Vector[-1, -1]
+  }
+
+  position = Vector[row, column]
+  row_count = rows.count
+  column_count = rows[0].length
+  count = 0
+  directions.each do |direction, step|
+    iterations = 1
+    loop do
+      neighbor = position + (step * iterations)
+      break if neighbor[0] < 0 || neighbor[0] >= row_count || neighbor[1] < 0 || neighbor[1] >= column_count
+      neighbor_line = rows[neighbor[0]]
+      spot = neighbor_line && neighbor_line[neighbor[1]]
+      count += 1 if spot == '#'
+      seat = spot == '#' || spot == 'L'
+      break if !spot || seat
+      iterations += 1
     end
   end
   count
@@ -84,5 +114,6 @@ Benchmark.bm do |benchmark|
   benchmark.report('Occupied seats (line of sight)'.light_blue) do
     count = count_occupied_seats(input, true)
     puts " (#{count})".green
+    puts ' --> ☠️'.red if count != 2054
   end
 end
