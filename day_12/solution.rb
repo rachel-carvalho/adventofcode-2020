4e2ff50110e1147a6f6cc464eaf1badafb50422c
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-12 11:52:02 -0500

    make directed ship class

--- day_12/solution.rb
+++ day_12/solution.rb
@@ -1,55 +1,66 @@
 require 'benchmark'
 require '../colors'
 
-@directions = {
-  'N' => [-1, 0],
-  'E' => [0, 1],
-  'S' => [1, 0],
-  'W' => [0, -1]
-}
+class DirectedShip
+  DIRECTIONS = {
+    'N' => [-1, 0],
+    'E' => [0, 1],
+    'S' => [1, 0],
+    'W' => [0, -1]
+  }.freeze
 
-def calculate_manhattan_position(instructions)
-  state = {
-    position: [0, 0],
-    direction: 'E'
-  }
+  attr_reader :position, :direction
 
-  instructions.each do |instruction|
-    move, value = instruction.scan(/(\w)(\d+)/).first
-    value = value.to_i
-
-    if %w[L R].include? move
-      rotate(state, move, value / 90)
-    else
-      direction = move == 'F' ? state[:direction] : move
-      move(state, direction, value)
-    end
+  def initialize
+    @position = [0, 0]
+    @direction = 'E'
   end
 
-  position = "#{readable_coordinate(state[:position][1], false)} #{readable_coordinate(state[:position][0], true)}"
+  def parse_movement(instructions)
+    instructions.each do |instruction|
+      movement, value = instruction.scan(/(\w)(\d+)/).first
+      value = value.to_i
 
-  [position, state[:position].map(&:abs).sum]
+      if %w[L R].include? movement
+        rotate(movement, value / 90)
+      else
+        move_direction = movement == 'F' ? @direction : movement
+        move(move_direction, value)
+      end
+    end
+    self
+  end
+
+  def to_s
+    "#{readable_coordinate(position[1], false)} #{readable_coordinate(position[0], true)} - #{manhattan_position}"
+  end
+
+  def manhattan_position
+    position.map(&:abs).sum
+  end
+
+  private
+
+  def readable_coordinate(point, vertical)
+    sign_name = point.positive? ? (vertical ? 'S' : 'E') : (vertical ? 'N' : 'W')
+    "#{point.abs}#{sign_name}"
+  end
+
+  def move(direction_name, value)
+    vector = DIRECTIONS[direction_name]
+    @position = [position[0] + vector[0] * value, position[1] + vector[1] * value]
+  end
+
+  def rotate(side, times)
+    sides = { 'R' => 1, 'L' => -1 }
+    steps = sides[side] * times
+    next_index = (DIRECTIONS.keys.index(direction) + steps) % DIRECTIONS.keys.count
+    @direction = DIRECTIONS.keys[next_index]
+  end
 end
 
-def readable_coordinate(point, vertical)
-  direction = point.positive? ? (vertical ? 'S' : 'E') : (vertical ? 'N' : 'W')
-  "#{point.abs}#{direction}"
-end
-
-def move(state, direction_name, value)
-  direction = @directions[direction_name]
-  state[:position] = [state[:position][0] + direction[0] * value, state[:position][1] + direction[1] * value]
-end
-
-def rotate(state, side, times)
-  sides = { 'R' => 1, 'L' => -1 }
-  steps = sides[side] * times
-  next_index = (@directions.keys.index(state[:direction]) + steps) % @directions.keys.count
-  state[:direction] = @directions.keys[next_index]
-end
-
-def answer_icon(result)
-  expected = ['17E 8S', 25]
+def answer_icon(result, waypoint = false)
+  expected = waypoint ? '214E 72S - 286' : '17E 8S - 25'
   result == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
 
@@ -61,15 +72,15 @@ R90
 F11'.split("\n")
 
 puts 'Example:'
-calculate_manhattan_position(example).tap { |result| puts "Manhattan position: #{result} #{answer_icon(result)}" }
+DirectedShip.new.parse_movement(example).tap { |result| puts "Manhattan position: #{result} #{answer_icon(result.to_s)}" }
 puts ''
 
 puts 'Input:'
 input = File.readlines('input')
 Benchmark.bm do |benchmark|
   benchmark.report('Manhattan position'.light_blue) do
-    position = calculate_manhattan_position(input)
-    puts " (#{position})".green
-    puts ' --> ☠️'.red if position != ["700E 1256S", 1956]
+    ship = DirectedShip.new.parse_movement(input)
+    puts " (#{ship})".green
+    puts ' --> ☠️'.red if ship.to_s != '700E 1256S - 1956'
   end
 end
