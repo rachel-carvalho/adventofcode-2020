bda82b1c53a59d38c62e31ae50f3d53ac95f2ce7
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-13 17:52:07 -0500

    add examples for part 2

--- day_13/solution.rb
+++ day_13/solution.rb
@@ -10,17 +10,34 @@ def first_bus_to_arrive(input)
   lines.first.merge(multiplied: (lines.first[:leaves] - availability) * lines.first[:line])
 end
 
-def answer_icon(result)
-  expected = {line: 59, leaves: 944, multiplied: 295}
+def first_time_for_sequence(input)
+  0
+end
+
+def answer_icon(result, index = nil)
+  answers = [3417, 754018, 779210, 1261476, 1202161486]
+  expected = index ? answers[index] : {line: 59, leaves: 944, multiplied: 295}
   result == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
 
-example =
+examples = [
 '939
-7,13,x,x,59,x,31,19'
+7,13,x,x,59,x,31,19',
+'0
+67,7,59,61',
+'0
+67,x,7,59,61',
+'0
+67,7,x,59,61',
+'0
+1789,37,47,1889'
+]
 
 puts 'Example:'
-first_bus_to_arrive(example).tap { |result| puts "First bus: #{result} #{answer_icon(result)}" }
+first_bus_to_arrive(examples[0]).tap { |result| puts "First bus: #{result} #{answer_icon(result)}" }
+examples.each_with_index do |example, index|
+  first_time_for_sequence(example).tap { |result| puts "Sequence timestamp #{index}: #{result} #{answer_icon(result, index)}" }
+end
 puts ''
 
 puts 'Input:'
