56d3a9b78f4a2c025a102ef1b35fa7b4236570f8
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-14 17:00:25 -0500

    apply mask to addresses

--- day_14/solution.rb
+++ day_14/solution.rb
@@ -27,7 +27,45 @@ def apply_mask(mask, value)
 end
 
 def mask_address_and_sum(program)
-  0
+  memory = {}
+  mask = nil
+  program.each do |line|
+    if line.include? 'mask'
+      mask = line[/(X|1|0){36}/]
+    else
+      index, value = line.scan(/mem\[(\d+)\] \= (\d+)/).first
+      masked_addresses(mask, index.to_i).each do |address|
+        memory[address] = value.to_i
+      end
+    end
+  end
+
+  memory.values.sum
+end
+
+def masked_addresses(mask, address)
+  mask_chars = mask.chars
+  binary = address.to_s(2).rjust(mask_chars.count, '0')
+  mask_chars.each_with_index do |char, index|
+    next if char == 'X'
+    binary[index] = (binary[index].to_i | char.to_i).to_s
+  end
+
+  items = [binary]
+
+  mask_chars.each_with_index do |char, index|
+    next if char != 'X'
+    new_items = []
+    items.each do |zero_bit|
+      one_bit = zero_bit.dup
+      zero_bit[index] = '0'
+      one_bit[index] = '1'
+      new_items << one_bit
+    end
+    items.concat new_items
+  end
+
+  items.map {|address| address.to_i(2) }
 end
 
 def answer_icon(result, address_mask = false)
@@ -63,5 +101,6 @@ Benchmark.bm do |benchmark|
   benchmark.report('Masked addresses sum'.light_blue) do
     result = mask_address_and_sum(input)
     puts " (#{result})".green
+    puts ' --> ☠️'.red if result != 4288986482164
   end
 end
