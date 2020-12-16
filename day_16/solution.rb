740a7ccf607d41cc1826353be7c5f81bb1438393
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-16 12:14:24 -0500

    parse own ticket and multiply departure fields

--- day_16/solution.rb
+++ day_16/solution.rb
@@ -14,7 +14,29 @@ class TicketParser
   end
 
   def identified
-    {}
+    possibilities = valid_tickets.transpose.map.with_index do |column, index|
+      possible_rules = rules.filter do |name, ranges|
+        column.all? { |number| ranges.any? { |range| range.include?(number) } }
+      end
+      [index, possible_rules.map(&:first)]
+    end
+
+    while possibilities.any? { |index, rule_names| rule_names.count > 1 }
+      possibilities.each do |index, rule_names|
+        if rule_names.count == 1
+          possibilities.each { |other_index, other_rules| other_index != index && other_rules.delete(rule_names.first) }
+        end
+      end
+    end
+
+    possibilities.map { |index, rule_names| [rule_names.first, own[index]] }.to_h
+  end
+
+  def rules
+    @rules ||= @sections[:rules].split("\n").map do |line|
+      name, range1_min, range1_max, range2_min, range2_max = line.scan(/\A(.+)\: (\d+)\-(\d+) or (\d+)\-(\d+)/).first
+      [name, [range1_min.to_i..range1_max.to_i, range2_min.to_i..range2_max.to_i]]
+    end.to_h
   end
 
   private
@@ -23,15 +45,8 @@ class TicketParser
     @nearby ||= parse_tickets(@sections[:nearby])
   end
 
-  def rules
-    @rules ||= @sections[:rules].split("\n").map do |line|
-      name, range1_min, range1_max, range2_min, range2_max = line.scan(/(\w+)\: (\d+)\-(\d+) or (\d+)\-(\d+)/).first
-      [name, [range1_min.to_i..range1_max.to_i, range2_min.to_i..range2_max.to_i]]
-    end.to_h
-  end
-
-  def valid_nearby
-    @valid_nearby ||= nearby.filter do |ticket|
+  def valid_tickets
+    @valid_tickets ||= nearby.filter do |ticket|
       ticket.all? do |number|
         rules.values.flatten.any? { |range| range.include?(number) }
       end
@@ -94,5 +109,16 @@ Benchmark.bm do |benchmark|
     result = TicketParser.new(input).invalid_values
     puts " (#{result.sum})".green
     puts ' --> ☠️'.red if result.sum != 23925
+  end
+
+  benchmark.report('Own ticket'.light_blue) do
+    parser = TicketParser.new(input)
+    result = parser.identified
+    departure_fields = result.filter { |field, value| field.start_with?('departure ') }
+    multiplied = departure_fields.values.reduce(:*)
+
+    puts " (#{result} - #{multiplied})".green
+    puts " --> ☠️ #{parser.rules.keys - result.keys}".red if result.keys.count != 20
+    puts ' --> ☠️'.red if multiplied != 964373157673
   end
 end
