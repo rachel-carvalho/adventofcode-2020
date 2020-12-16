3ed437ed10ba17b94a776650788830c0a9243093
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-16 11:38:57 -0500

    refactor to identify fields

--- day_16/solution.rb
+++ day_16/solution.rb
@@ -2,13 +2,9 @@ require 'benchmark'
 require '../colors'
 
 class TicketParser
-  attr_reader :rules, :own, :nearby
-
   def initialize(input)
-    rule_definition, own, nearby = input.split "\n\n"
-    @rules = parse_rules(rule_definition)
-    @own = parse_tickets(own)
-    @nearby = parse_tickets(nearby)
+    rules, own, nearby = input.split "\n\n"
+    @sections = { rules: rules, own: own, nearby: nearby }
   end
 
   def invalid_values
@@ -17,13 +13,33 @@ class TicketParser
     end
   end
 
+  def identified
+    {}
+  end
+
   private
 
-  def parse_rules(definition)
-    definition.split("\n").map do |line|
+  def nearby
+    @nearby ||= parse_tickets(@sections[:nearby])
+  end
+
+  def rules
+    @rules ||= @sections[:rules].split("\n").map do |line|
       name, range1_min, range1_max, range2_min, range2_max = line.scan(/(\w+)\: (\d+)\-(\d+) or (\d+)\-(\d+)/).first
       [name, [range1_min.to_i..range1_max.to_i, range2_min.to_i..range2_max.to_i]]
     end.to_h
+  end
+
+  def valid_nearby
+    @valid_nearby ||= nearby.filter do |ticket|
+      ticket.all? do |number|
+        rules.values.flatten.any? { |range| range.include?(number) }
+      end
+    end
+  end
+
+  def own
+    @own ||= parse_tickets(@sections[:own]).first
   end
 
   def parse_tickets(input)
@@ -33,8 +49,8 @@ class TicketParser
   end
 end
 
-def answer_icon(result)
-  expected = [4, 55, 12]
+def answer_icon(result, parsed = false)
+  expected = parsed ? { 'class' => 12, 'row' => 11, 'seat' => 13 } : [4, 55, 12]
   result == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
 
@@ -52,8 +68,23 @@ nearby tickets:
 55,2,20
 38,6,12'
 
+example2 =
+'class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+55,2,20'
+
 puts 'Example:'
 TicketParser.new(example).invalid_values.tap { |result| puts "Invalid values sum: #{result} - #{result.sum} #{answer_icon(result)}" }
+TicketParser.new(example2).identified.tap { |result| puts "Own ticket: #{result} #{answer_icon(result, true)}" }
 puts ''
 
 puts 'Input:'
