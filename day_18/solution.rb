8314461520f4a7cc7bde54a0566e0180a3a81c0f
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-18 12:39:46 -0500

    calculate with parenthesis

--- day_18/solution.rb
+++ day_18/solution.rb
@@ -8,13 +8,26 @@ def sum_weird_arithmetic(expressions)
 end
 
 def solve_weird_arithmetic(expression)
-  regexp = /(\d+) (\+|\*) (\d+)/
-  while part = expression[regexp]
-    operand1, operator, operand2 = part.scan(regexp).first
-    result = operator == '+' ? (operand1.to_i + operand2.to_i) : (operand1.to_i * operand2.to_i)
-    expression[regexp] = result.to_s
+  last_level_parenthesis = /\(\d+( (\*|\+) \d+)+\)/
+  while parenthesis = expression[last_level_parenthesis]
+    parenthesis = parenthesis[1...-1]
+    calculate_simple_operations(parenthesis)
+    expression[last_level_parenthesis] = parenthesis
   end
+  calculate_simple_operations(expression)
   expression.to_i
+end
+
+def calculate_simple_operations(expression)
+  simple_operation = /(\d+) (\+|\*) (\d+)/
+  while part = expression[simple_operation]
+    result = calculate_simple_operation(*part.scan(simple_operation).first)
+    expression[simple_operation] = result.to_s
+  end
+end
+
+def calculate_simple_operation(operand1, operator, operand2)
+  operator == '+' ? (operand1.to_i + operand2.to_i) : (operand1.to_i * operand2.to_i)
 end
 
 def answer_icon(result, index)
@@ -41,8 +54,9 @@ puts ''
 puts 'Input:'
 input = File.readlines('input')
 Benchmark.bm do |benchmark|
-  benchmark.report('Invalid values sum'.light_blue) do
+  benchmark.report('Result'.light_blue) do
     result = sum_weird_arithmetic(input)
     puts " (#{result})".green
+    puts ' --> ☠️'.red if result != 5019432542701
   end
 end
