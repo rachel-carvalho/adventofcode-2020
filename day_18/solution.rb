5651cbf82f9ae01452afbad87523d64ad044a820
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-18 12:49:21 -0500

    solve with addition precedence

--- day_18/solution.rb
+++ day_18/solution.rb
@@ -1,28 +1,33 @@
 require 'benchmark'
 require '../colors'
+require 'pry'
 
-def sum_weird_arithmetic(expressions)
+def sum_weird_arithmetic(expressions, addition_precedence = false)
   expressions.sum do |expression|
-    solve_weird_arithmetic(expression.strip)
+    solve_weird_arithmetic(expression.strip, addition_precedence)
   end
 end
 
-def solve_weird_arithmetic(expression)
+def solve_weird_arithmetic(expression, addition_precedence = false)
   last_level_parenthesis = /\(\d+( (\*|\+) \d+)+\)/
   while parenthesis = expression[last_level_parenthesis]
     parenthesis = parenthesis[1...-1]
-    calculate_simple_operations(parenthesis)
+    calculate_simple_operations(parenthesis, addition_precedence)
     expression[last_level_parenthesis] = parenthesis
   end
-  calculate_simple_operations(expression)
+  calculate_simple_operations(expression, addition_precedence)
   expression.to_i
 end
 
-def calculate_simple_operations(expression)
+def calculate_simple_operations(expression, addition_precedence)
   simple_operation = /(\d+) (\+|\*) (\d+)/
-  while part = expression[simple_operation]
-    result = calculate_simple_operation(*part.scan(simple_operation).first)
-    expression[simple_operation] = result.to_s
+  operations = [simple_operation]
+  operations.prepend(/(\d+) (\+) (\d+)/) if addition_precedence
+  operations.each do |operation|
+    while part = expression[operation]
+      result = calculate_simple_operation(*part.scan(operation).first)
+      expression[operation] = result.to_s
+    end
   end
 end
 
@@ -30,8 +35,8 @@ def calculate_simple_operation(operand1, operator, operand2)
   operator == '+' ? (operand1.to_i + operand2.to_i) : (operand1.to_i * operand2.to_i)
 end
 
-def answer_icon(result, index)
-  answers = [71, 51, 26, 437, 12240, 13632]
+def answer_icon(result, index, addition_precedence = false)
+  answers = addition_precedence ? [231, 51, 46, 1445, 669060, 23340] : [71, 51, 26, 437, 12240, 13632]
   expected = answers[index]
   result == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
@@ -47,7 +52,8 @@ examples = [
 
 puts 'Example:'
 examples.each_with_index do |expression, index|
-  solve_weird_arithmetic(expression).tap { |result| puts "Result: #{result} #{answer_icon(result, index)}" }
+  solve_weird_arithmetic(expression.dup).tap { |result| puts "Result: #{result} #{answer_icon(result, index)}" }
+  solve_weird_arithmetic(expression.dup, true).tap { |result| puts "Addition precedence: #{result} #{answer_icon(result, index, true)}" }
 end
 puts ''
 
@@ -58,5 +64,10 @@ Benchmark.bm do |benchmark|
     result = sum_weird_arithmetic(input)
     puts " (#{result})".green
     puts ' --> ☠️'.red if result != 5019432542701
+  end
+  benchmark.report('Addition precedence'.light_blue) do
+    result = sum_weird_arithmetic(input, true)
+    puts " (#{result})".green
+    puts ' --> ☠️'.red if result != 70518821989947
   end
 end
