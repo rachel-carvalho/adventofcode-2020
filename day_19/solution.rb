a898f30415e7a3eccb870981dd9cee7bdb4ed80d
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-12-19 18:42:04 -0500

    parse rule and count matches

--- day_19/solution.rb
+++ day_19/solution.rb
@@ -1,12 +1,41 @@
 require 'benchmark'
 require '../colors'
+require 'pry'
 
 def count_matches(input)
-  0
+  rule_section, messages = input.split("\n\n")
+
+  rule = first_rule(rule_section)
+
+  messages.split("\n").count do |message|
+    message.strip!
+    regexp = Regexp.new("\\A#{rule}\\z")
+    message.match? regexp
+  end
 end
 
-def answer_icon(result)
-  expected = 2
+def first_rule(input)
+  rules = input.strip.split("\n").map do |line|
+    index, rule = line.scan(/(\d+)\: (.*)/).first
+
+    rule.gsub!('"', '') if rule.include?('"')
+    rule = "(#{rule})" if rule.include?('|')
+
+    [index, rule]
+  end.to_h
+
+  rule = rules['0']
+
+  while reference_index = rule[/\d+/]
+    rule[/\d+/] = rules[reference_index]
+  end
+
+  rule.gsub!(' ', '')
+end
+
+def answer_icon(result, test_rule = false)
+  rule = 'a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b'
+  expected = test_rule ? rule : 2
   result == expected ? '✔'.green : '✗'.red + " expected #{expected}"
 end
 
@@ -25,6 +54,7 @@ aaabbb
 aaaabbb'
 
 puts 'Example:'
+first_rule(example.split("\n\n").first).tap { |result| puts "First rule: #{result} #{answer_icon(result, true)}" }
 count_matches(example).tap { |result| puts "Rule 0 matches: #{result} #{answer_icon(result)}" }
 puts ''
 
@@ -34,5 +64,6 @@ Benchmark.bm do |benchmark|
   benchmark.report('Rule 0 matches'.light_blue) do
     result = count_matches(input)
     puts " (#{result})".green
+    puts ' --> ☠️'.red if result != 241
   end
 end
